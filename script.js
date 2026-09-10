5d4b436152b176c3a9ca425d939cd3e774e40670
waec2022 <ubivictor745@gmail.com>
2026-09-10 13:37:11 +0100

    Add alert on copy button click in accumulator card
    Updated the return statement to include an alert when the copy button is clicked.

--- script.js
+++ script.js
@@ -277,9 +277,9 @@ function renderAccumulators(list) {
       var avgOdds = m.bookmakers.reduce(function(sum, b){ return sum + b.odds; }, 0) / m.bookmakers.length;
       return "<div class=\"accumulator-card_match\"><span class=\"accumulator-card_match-teams\">" + m.match + "</span><span class=\"accumulator-card_match-pick\">" + m.pick + " @ " + avgOdds.toFixed(2) + "</span></div>";
     }).join("");
-    return "<div class=\"accumulator-card " + a.themeClass + "\"><div class=\"accumulator-card_odds\">" + a.odds.toUpperCase() + "</div><div class=\"accumulator-card_desc\">" + matches.length + " Selections - Combined " + combinedOdds.toFixed(2) + "</div><div class=\"accumulator-card_matches\">" + matchesHTML + "</div><div class=\"accumulator-card_code\"><b>Code:</b> " + a.combinedCode + "</div><button class=\"accumulator-card_btn\" onclick=\"navigator.clipboard.writeText('" + a.combinedCode + "')\">Copy Code</button></div>";
+    return "<div class=\"accumulator-card " + a.themeClass + "\"><div class=\"accumulator-card_odds\">" + a.odds.toUpperCase() + "</div><div class=\"accumulator-card_desc\">" + matches.length + " Selections - Combined " + combinedOdds.toFixed(2) + "</div><div class=\"accumulator-card_matches\">" + matchesHTML + "</div><div class=\"accumulator-card_code\"><b>Code:</b> " + a.combinedCode + "</div><button class=\"accumulator-card_btn\" onclick=\"navigator.clipboard.writeText('" + a.combinedCode + "'); alert('Copied: " + a.combinedCode + "')\">Copy Code</button></div>";
   }).join("");
-                                          }
+       }                                          }
 
   function renderResults(results) {
     var grid = document.getElementById("resultsGrid");
