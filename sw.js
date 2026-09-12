bdf66aee9b28dc384cea0122b229c1cdc6013c7b
waec2022 <ubivictor745@gmail.com>
2026-09-12 09:37:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,19 @@
-self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11750514
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+// This service worker previously loaded third-party (Monetag) ad/push code.
+// That has been disabled. This version's only job is to remove itself and
+// any caches it created from visitors who already had it installed, then
+// stop running, so the site goes back to normal browser networking/caching.
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    (async () => {
+      const keys = await caches.keys();
+      await Promise.all(keys.map((key) => caches.delete(key)));
+      await self.registration.unregister();
+      const clientsList = await self.clients.matchAll({ type: 'window' });
+      clientsList.forEach((client) => client.navigate(client.url));
+    })()
+  );
+});
